250b720ee2915057b640f22691f70c4300625a5d
Filipe Arruda <filipe@Filipe-Arruda.local>
2026-06-20 17:19:39 -0300

    Initial commit

---
+++ src/main/java/com/filipearruda/tech_challenge_restaurant_manager_phase_two/TechChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.filipearruda.tech_challenge_restaurant_manager_phase_two;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/filipearruda/tech_challenge_restaurant_manager_phase_two/TestcontainersConfiguration.java
@@ -0,0 +1,18 @@
+package com.filipearruda.tech_challenge_restaurant_manager_phase_two;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer postgresContainer() {
+        return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+    }
+
+}
